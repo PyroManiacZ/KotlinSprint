04158393e7e033c2938315a5447629fc191d12cb
Nikolay <wolk20000@g,ail.com>
2025-06-04 14:59:23 +0300

    KS-1-3 "$hour"

--- src/main/kotlin/lession1/lession1_task3.kt
+++ src/main/kotlin/lession1/lession1_task3.kt
@@ -1,6 +1,16 @@
 package org.example.lession1
 
 fun main() {
-    println("Решение тестовой задачи к 1 уроку задача 3")
-    println("доработки")
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println(year)
+//    println(hour)
+//    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println("$hour:$minute")
 }
