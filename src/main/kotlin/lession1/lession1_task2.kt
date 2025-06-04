7554926d8466051306fb61e7a3c0e8d71cb39d89
Nikolay <wolk20000@g,ail.com>
2025-06-04 14:51:57 +0300

    KS-1-2 var -1

--- src/main/kotlin/lession1/lession1_task2.kt
+++ src/main/kotlin/lession1/lession1_task2.kt
@@ -1,6 +1,18 @@
 package org.example.lession1
 
 fun main() {
-    println("Решение тестовой задачи к 1 уроку задача 2")
-    println("доработки")
+
+    val numberOfOrders = 75
+    val thanksPurchase = "Благодарим за покупку нашего продукта"
+
+    println(numberOfOrders)
+    println(thanksPurchase)
+
+    var numberOfWorkers = 2000
+
+//    println(numberOfWorkers)
+
+    numberOfWorkers = 1999
+
+    println(numberOfWorkers)
 }
