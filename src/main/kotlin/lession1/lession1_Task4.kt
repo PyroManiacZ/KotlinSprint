725da21857ce28fc91e174d9b64976f11a2bc396
Nikolay <wolk20000@g,ail.com>
2025-06-09 14:41:45 +0300

    KS-1-4-smallest-variables

---
+++ src/main/kotlin/lession1/lession1_Task4.kt
@@ -0,0 +1,20 @@
+package org.example.lession1
+
+fun main() {
+
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("Расстояние: $length")
+    println("Возраст: $age")
+    println("Чась дня: $partOfDay")
+    println("Секунды: $seconds")
+    println("Часть года: $partOfYear")
+    println("Апогей орбиты: $apogee")
+
+}
+
